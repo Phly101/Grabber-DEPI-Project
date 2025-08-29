88bc4609396d132597990503b91f83d36749b1a3
martinawageeh <martinawageeh038@gmail.com>
2025-08-29 17:28:01 +0300

    + add product_card + add product_data + add product_model + add fruit_items + add detergent_items + add biscuit_items % edit home_tab

--- lib/UI/home/home_tab.dart
+++ lib/UI/home/home_tab.dart
@@ -1,12 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:grabber_app/Theme/light_theme.dart';
+import 'widgetT/biscuit_items.dart';
+import 'widgetT/detergent_items.dart';
+import 'widgetT/fruit_items.dart';
+
+//TODO: make FloatingActionButton
+//TODO: make Navigator
 
 class HomeTab extends StatelessWidget {
   static const String routeName = "home_tab";
-
   const HomeTab({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Home Page...."));
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+              children: [
+                  Row(
+                    children: [
+                      Text(
+                        'Fruits',
+                        style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                      ),
+                      Spacer(),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'See all',
+                          style: TextStyle(color: LightThemeData.darkPrimaryColor,fontSize: 20,fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    ],
+                  ),
+                SizedBox(height: 10),
+                AspectRatio(            //card maintains its shape on any screen.
+                  aspectRatio: 3 / 2,
+                  child: FruitsItems(),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text(
+                      'Detergent',
+                      style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                    ),
+                    Spacer(),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'See all',
+                        style: TextStyle(color: LightThemeData.darkPrimaryColor,fontSize: 20,fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 5),
+                AspectRatio(            //card maintains its shape on any screen.
+                  aspectRatio: 3 / 2,
+                  child:DetergentItems(),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text(
+                      'Biscuit',
+                      style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                    ),
+                    Spacer(),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'See all',
+                        style: TextStyle(color: LightThemeData.darkPrimaryColor,fontSize: 20,fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 10),
+                AspectRatio(            //card maintains its shape on any screen.
+                  aspectRatio: 3 / 2,
+                  child:BiscuitItems(),
+                  ),
+
+              ],
+            ),
+        ),
+      ),
+      //),
+    );
   }
 }
